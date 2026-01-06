e7a79715e8436d533c66f5c810c011498c52c681
thebiggonsv3 <zhang.ryan8976@gmail.com>
2026-01-06 11:08:56 -0500

    Initial setup and formatting

---
+++ script.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	char password[100];
+	int strength;
+
+	printf("thebiggonsv3's Password Strength Schecker\n");
+	printf("------------------------------------------\n");
+	printf("Welcome! To get started, please enter a \npassword you would like to test.\n");
+
+	scanf("%s", password);
+	printf("------------------------------------------\n");
+
+	strength = 0;
+
+
+	return 0;
+}
